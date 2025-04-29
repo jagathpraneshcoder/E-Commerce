7a05fd74c579afbb2d05885f5031b64f54bf185d
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2025-04-29 16:29:24 +0530

    Product Model

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'user'], default: 'user' },
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    image: { type: String, required: true }, 
+    review: { type: String }, 
+    details: { type: String },
+    price: { type: Number, required: true },
+}, {
+    timestamps: true, 
 });
 
-const User = mongoose.model('User', userSchema);
+const Product = mongoose.model('Product', productSchema);
 
-export default User;
+export default Product;

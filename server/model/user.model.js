0fd5f97b56500a31f446137d39bf98de2793177e
JAGATHPRANESH(aka)ASWIN <115636703+jagathpraneshcoder@users.noreply.github.com>
2025-04-30 16:22:10 +0530

    Update user.model.js
    user model

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -1,15 +1,56 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt'); 
+const Schema =mongoose.Schema
+const validator = require('validator')
 
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    image: { type: String, required: true }, 
-    review: { type: String }, 
-    details: { type: String },
-    price: { type: Number, required: true },
-}, {
-    timestamps: true, 
+const userSchema = new Schema({
+  email:{
+    type:String,
+    required: true,
+    unique:true
+  },
+  password:{
+    type:String,
+    required:true
+  }
 });
 
-const Product = mongoose.model('Product', productSchema);
 
-export default Product;
+userSchema.statics.login = async function(email,password){
+  if(!email || !password){
+    throw Error('All Fields are required!');
+  }
+  const user = await this.findOne({email});
+  if(!user){
+    throw Error('Email Not Exist');
+  }
+  const match = await bcrypt.compare(password,user.password);
+  if(!match){
+    throw Error('Invalid Password');
+  }
+  return user;
+}
+
+
+userSchema.statics.signUp = async function(email,password){
+    if(!email || !password){
+      throw Error('All Fields Are Required!');
+    }
+    if(!validator.isEmail(email)){
+      throw Error('Email is not Valid');
+    }
+    const exist = await this.findOne({email});
+    if(exist){
+      throw Error('Email Already in Use');
+    }
+    if(!validator.isStrongPassword(password)){
+      throw Error('Password is not Strong!');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password,salt);
+
+    const user = await this.create({email,password:hash});
+    return user;
+}
+
+module.exports = mongoose.model('User',userSchema);
